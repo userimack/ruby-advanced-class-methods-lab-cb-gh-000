1dc0e06133c7f0425105cde6275e469ed5c2bd89
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-08 12:24:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
     self.all.sort_by {|song| song.name}
   end 
   
-  def self.create_from_filename(filename)
+  def self.new_from_filename(filename)
     artist_name, song_name = filename.split(" - ")
     song_name = song_name.split(".mp3")[0]
     song = self.create()
